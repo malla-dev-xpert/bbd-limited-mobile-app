168052e6fbec28ea6c7b6d9853fcbc8777377c86
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-04-14 17:58:09 +0000

    retreive duplicate card in basic screen

--- lib/components/basic/card_list.dart
+++ lib/components/basic/card_list.dart
@@ -48,58 +48,7 @@ List<CardData> cardDataList = [
     titleColor: const Color(0xFF13084F),
     descriptionColor: Colors.black54,
     onPressed: (context) {
-      Navigator.of(context).pushNamed('/home');
-    },
-  ),
-  CardData(
-    icon: Icons.info,
-    title: 'Gestion des ports',
-    description: 'Ajout et gestion des informations spécifiques aux ports',
-    backgroundColor: Colors.grey[50]!,
-    iconColor: const Color(0xFF13084F),
-    titleColor: const Color(0xFF13084F),
-    descriptionColor: Colors.black54,
-    onPressed: (context) {
-      Navigator.of(context).pushNamed('/home');
-    },
-  ),
-  CardData(
-    icon: Icons.directions_transit,
-    title: 'Gestion des transporteurs',
-    description:
-        'Suivi des partenaires logistiques et gestion des prestataires de transport.',
-    backgroundColor: Colors.grey[50]!,
-    iconColor: const Color(0xFF13084F),
-    titleColor: const Color(0xFF13084F),
-    descriptionColor: Colors.black54,
-    onPressed: (context) {
-      Navigator.of(context).pushNamed('/home');
-    },
-  ),
-  CardData(
-    icon: Icons.monetization_on_rounded,
-    title: 'Gestion des devises',
-    description:
-        'Paramétrage des monnaies utilisées pour les transactions internationales.',
-    backgroundColor: Colors.grey[50]!,
-    iconColor: const Color(0xFF13084F),
-    titleColor: const Color(0xFF13084F),
-    descriptionColor: Colors.black54,
-    onPressed: (context) {
-      Navigator.of(context).pushNamed('/home');
-    },
-  ),
-  CardData(
-    icon: Icons.warehouse,
-    title: 'Gestion des entrepôts',
-    description:
-        'Enregistrement et suivi des entrepôts pour le stockage des marchandises.',
-    backgroundColor: Colors.grey[50]!,
-    iconColor: const Color(0xFF13084F),
-    titleColor: const Color(0xFF13084F),
-    descriptionColor: Colors.black54,
-    onPressed: (context) {
-      Navigator.of(context).pushNamed('/home');
+      Navigator.of(context).pushNamed('/warehouse');
     },
   ),
   CardData(
