3e8bccc413aad7fd553677fd3453d242f82eb201
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-04-25 11:18:21 +0000

    update fetchharbor to add query option

--- lib/screens/gestion/basics/subScreens/harbor/harbor_screen.dart
+++ lib/screens/gestion/basics/subScreens/harbor/harbor_screen.dart
@@ -40,7 +40,7 @@ class _HarborScreen extends State<HarborScreen> {
     super.dispose();
   }
 
-  Future<void> fetchHarbor({bool reset = false}) async {
+  Future<void> fetchHarbor({bool reset = false, String? searchQuery}) async {
     if (_isLoading || (!reset && !_hasMoreData)) return;
 
     setState(() {
@@ -52,14 +52,26 @@ class _HarborScreen extends State<HarborScreen> {
       }
     });
     try {
-      final result = await _harborServices.findAll(page: currentPage);
+      final result = await _harborServices.findAll(
+        page: currentPage,
+        query: searchQuery,
+      );
 
       setState(() {
-        if (reset) {
-          _allHarbor.clear();
-        }
         _allHarbor.addAll(result);
-        _filteredHarbor = List.from(_allHarbor);
+
+        _filteredHarbor =
+            _allHarbor
+                .where(
+                  (port) =>
+                      searchQuery == null || searchQuery.isEmpty
+                          ? true
+                          : (port.name?.toLowerCase().contains(
+                                searchQuery.toLowerCase(),
+                              ) ??
+                              false),
+                )
+                .toList();
 
         if (result.isEmpty || result.length < 30) {
           _hasMoreData = false;
@@ -75,17 +87,32 @@ class _HarborScreen extends State<HarborScreen> {
     }
   }
 
-  void searchHarbor(String query) {
-    setState(() {
-      _filteredHarbor =
-          _allHarbor.where((port) {
-            final searchPackage = port.name!.toLowerCase().contains(
-              query.toLowerCase(),
-            );
+  void searchHarbor(String query) async {
+    // recherche locale
+    final localResults =
+        _allHarbor.where((port) {
+          return port.name?.toLowerCase().contains(query.toLowerCase()) ??
+              false;
+        }).toList();
 
-            return searchPackage;
-          }).toList();
-    });
+    if (localResults.isNotEmpty) {
+      setState(() => _filteredHarbor = localResults);
+    } else {
+      // recherche dans la base de donnee
+      try {
+        await fetchHarbor(reset: true, searchQuery: query);
+
+        final newResults =
+            _allHarbor.where((port) {
+              return port.name?.toLowerCase().contains(query.toLowerCase()) ??
+                  false;
+            }).toList();
+
+        setState(() => _filteredHarbor = newResults);
+      } catch (e) {
+        showErrorTopSnackBar(context, "Erreur lors de la recherche");
+      }
+    }
   }
 
   @override
@@ -147,96 +174,120 @@ class _HarborScreen extends State<HarborScreen> {
             const SizedBox(height: 20),
 
             Expanded(
-              child: ListView.builder(
-                itemCount: _filteredHarbor.length,
-                itemBuilder: (context, index) {
-                  return GridView.count(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 4,
-                    crossAxisSpacing: 4,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    children: List.generate(_filteredHarbor.length, (index) {
-                      final port = _filteredHarbor[index];
+              child: NotificationListener<ScrollNotification>(
+                onNotification: (scrollInfo) {
+                  if (scrollInfo.metrics.pixels ==
+                          scrollInfo.metrics.maxScrollExtent &&
+                      !_isLoading &&
+                      _hasMoreData) {
+                    fetchHarbor();
+                  }
+                  return false;
+                },
+                child: ListView.builder(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  itemCount:
+                      _filteredHarbor.length +
+                      (_hasMoreData && _isLoading ? 1 : 0),
+                  itemBuilder: (context, index) {
+                    return GridView.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      children: List.generate(_filteredHarbor.length, (index) {
+                        final port = _filteredHarbor[index];
+                        if (_allHarbor == []) {
+                          return Center(child: CircularProgressIndicator());
+                        }
 
-                      return Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        color: Colors.grey[50],
-                        elevation: 2,
-                        child: Column(
-                          children: [
-                            // Image de fond avec overlay sombre
-                            Stack(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: const BorderRadius.vertical(
-                                    top: Radius.circular(20),
-                                  ),
-                                  child: Image.asset(
-                                    "assets/images/ports.jpg",
-                                    height: 70,
-                                    width: double.infinity,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                Container(
-                                  height: 70,
-                                  decoration: BoxDecoration(
+                        if (_filteredHarbor.isEmpty) {
+                          return Center(child: Text("Aucun port trouvé"));
+                        }
+
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          color: Colors.grey[50],
+                          elevation: 2,
+                          child: Column(
+                            children: [
+                              // Image de fond avec overlay sombre
+                              Stack(
+                                children: [
+                                  ClipRRect(
                                     borderRadius: const BorderRadius.vertical(
                                       top: Radius.circular(20),
                                     ),
-                                    color: Colors.black.withOpacity(0.4),
+                                    child: Image.asset(
+                                      "assets/images/ports.jpg",
+                                      height: 70,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                  alignment: Alignment.center,
-                                  child: const Icon(
-                                    Icons.local_shipping,
-                                    size: 30,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ],
-                            ),
-
-                            // Informations du port
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      port.name!,
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 14,
-                                        letterSpacing: -1,
+                                  Container(
+                                    height: 70,
+                                    decoration: BoxDecoration(
+                                      borderRadius: const BorderRadius.vertical(
+                                        top: Radius.circular(20),
                                       ),
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
+                                      color: Colors.black.withOpacity(0.4),
                                     ),
-                                    const SizedBox(height: 4),
-                                    Text(
-                                      "Adresse : ${port.location ?? 'Non spécifiée'}",
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: const TextStyle(letterSpacing: -1),
+                                    alignment: Alignment.center,
+                                    child: const Icon(
+                                      Icons.local_shipping,
+                                      size: 30,
+                                      color: Colors.white,
                                     ),
-                                    const SizedBox(height: 4),
-                                    Text(
-                                      "Conteneurs : ${port.containers?.length ?? 0}",
-                                    ),
-                                  ],
+                                  ),
+                                ],
+                              ),
+
+                              // Informations du port
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        port.name!,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 14,
+                                          letterSpacing: -1,
+                                        ),
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                      const SizedBox(height: 4),
+                                      Text(
+                                        "Adresse : ${port.location ?? 'Non spécifiée'}",
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          letterSpacing: -1,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 4),
+                                      Text(
+                                        "Conteneurs : ${port.containers?.length ?? 0}",
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
-                  );
-                },
+                            ],
+                          ),
+                        );
+                      }),
+                    );
+                  },
+                ),
               ),
             ),
           ],
