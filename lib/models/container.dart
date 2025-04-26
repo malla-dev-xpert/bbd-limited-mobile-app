7917207a0e40a0a2d6dbba996eb638724c85dc89
Mohamed Lamine KONE <konelamine1122@gmail.com>
2025-04-26 09:47:59 +0000

    get container status

--- lib/models/container.dart
+++ lib/models/container.dart
@@ -1,12 +1,32 @@
+import 'package:bbd_limited/core/enums/status.dart';
+
 class Containers {
   final String? reference;
   final DateTime? createdAt;
   final DateTime? editedAt;
   final bool? isAvailable;
+  final Status? status;
 
-  Containers({this.reference, this.createdAt, this.editedAt, this.isAvailable});
+  Containers({
+    this.reference,
+    this.createdAt,
+    this.editedAt,
+    this.isAvailable,
+    this.status,
+  });
 
   factory Containers.fromJson(Map<String, dynamic> json) {
+    String? statusString = json['status'];
+    Status status;
+
+    if (statusString != null) {
+      status = Status.values.firstWhere(
+        (e) => e.name.toUpperCase() == statusString.toUpperCase(),
+        orElse: () => Status.CREATE,
+      );
+    } else {
+      status = Status.CREATE;
+    }
     return Containers(
       reference: json['reference'] as String?,
       createdAt:
@@ -14,6 +34,7 @@ class Containers {
       editedAt:
           json['editedAt'] != null ? DateTime.parse(json['editedAt']) : null,
       isAvailable: json['isAvailable'] as bool?,
+      status: status,
     );
   }
 }
