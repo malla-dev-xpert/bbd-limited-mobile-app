448e6f08ac08d50505d434b546fc583c07813066
MallaDev <konelamine1122@gmail.com>
2026-01-26 16:29:47 +0000

    💄 Enlever la statistique a l'accueil

--- lib/screens/gestion/basics/basic_home_screen.dart
+++ lib/screens/gestion/basics/basic_home_screen.dart
@@ -1,11 +1,11 @@
 import 'package:bbd_limited/components/basic/card_list.dart';
-import 'package:bbd_limited/components/basic/report/report_card.dart';
-import 'package:bbd_limited/components/basic/report/report_card_list.dart';
+// import 'package:bbd_limited/components/basic/report/report_card.dart';
+// import 'package:bbd_limited/components/basic/report/report_card_list.dart';
 import 'package:bbd_limited/core/services/auth_services.dart';
 import 'package:flutter/material.dart';
 import '../../../models/user.dart';
-import 'package:bbd_limited/core/services/container_services.dart';
-import 'package:bbd_limited/core/enums/status.dart';
+// import 'package:bbd_limited/core/services/container_services.dart';
+// import 'package:bbd_limited/core/enums/status.dart';
 import 'package:bbd_limited/core/localization/app_localizations.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,15 +18,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final AuthService _authService = AuthService();
   User? _user;
-  final ContainerServices _containerServices = ContainerServices();
-  int _expeditionsEnCours = 0;
-  int _totalColisEnTransit = 0;
+  // final ContainerServices _containerServices = ContainerServices();
+  // int _expeditionsEnCours = 0;
+  // int _totalColisEnTransit = 0;
 
   @override
   void initState() {
     super.initState();
     _loadUserInfo();
-    _loadExpeditionsStats();
+    // _loadExpeditionsStats();
   }
 
   Future<void> _loadUserInfo() async {
@@ -38,55 +38,55 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  Future<void> _loadExpeditionsStats() async {
-    setState(() {
-      _expeditionsEnCours = 0;
-      _totalColisEnTransit = 0;
-    });
-    try {
-      final containers = await _containerServices.findAll(page: 0);
-      final inProgressContainers =
-          containers.where((c) => c.status == Status.INPROGRESS).toList();
-      int totalPackages = 0;
-      for (final container in inProgressContainers) {
-        totalPackages += container.packages?.length ?? 0;
-      }
-      if (mounted) {
-        setState(() {
-          _expeditionsEnCours = inProgressContainers.length;
-          _totalColisEnTransit = totalPackages;
-        });
-      }
-    } catch (e) {
-      if (mounted) {
-        setState(() {
-          _expeditionsEnCours = 0;
-          _totalColisEnTransit = 0;
-        });
-      }
-    }
-  }
+  // Future<void> _loadExpeditionsStats() async {
+  //   setState(() {
+  //     _expeditionsEnCours = 0;
+  //     _totalColisEnTransit = 0;
+  //   });
+  //   try {
+  //     final containers = await _containerServices.findAll(page: 0);
+  //     final inProgressContainers =
+  //         containers.where((c) => c.status == Status.INPROGRESS).toList();
+  //     int totalPackages = 0;
+  //     for (final container in inProgressContainers) {
+  //       totalPackages += container.packages?.length ?? 0;
+  //     }
+  //     if (mounted) {
+  //       setState(() {
+  //         _expeditionsEnCours = inProgressContainers.length;
+  //         _totalColisEnTransit = totalPackages;
+  //       });
+  //     }
+  //   } catch (e) {
+  //     if (mounted) {
+  //       setState(() {
+  //         _expeditionsEnCours = 0;
+  //         _totalColisEnTransit = 0;
+  //       });
+  //     }
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context);
 
-    final List<ReportCardData> dynamicReportCardDataList = [
-      ReportCardData(
-        title: localizations.translate('home_expeditions_in_progress'),
-        value: _expeditionsEnCours.toString(),
-        backgroundColor: Colors.blue[800]!,
-        textColor: Colors.white,
-        icon: Icons.local_shipping,
-      ),
-      ReportCardData(
-        title: localizations.translate('home_packages_in_transit'),
-        value: _totalColisEnTransit.toString(),
-        backgroundColor: Colors.orange[800]!,
-        textColor: Colors.white,
-        icon: Icons.inventory_2_rounded,
-      ),
-    ];
+    // final List<ReportCardData> dynamicReportCardDataList = [
+    //   ReportCardData(
+    //     title: localizations.translate('home_expeditions_in_progress'),
+    //     value: _expeditionsEnCours.toString(),
+    //     backgroundColor: Colors.blue[800]!,
+    //     textColor: Colors.white,
+    //     icon: Icons.local_shipping,
+    //   ),
+    //   ReportCardData(
+    //     title: localizations.translate('home_packages_in_transit'),
+    //     value: _totalColisEnTransit.toString(),
+    //     backgroundColor: Colors.orange[800]!,
+    //     textColor: Colors.white,
+    //     icon: Icons.inventory_2_rounded,
+    //   ),
+    // ];
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -141,37 +141,37 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ],
                     ),
-                    const SizedBox(height: 20),
-                    Text(
-                      localizations.translate('home_statistics'),
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: -1,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      physics: const BouncingScrollPhysics(),
-                      child: Row(
-                        children: dynamicReportCardDataList.map((data) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 3.0,
-                            ),
-                            child: ReportCard(
-                              title: data.title,
-                              value: data.value,
-                              backgroundColor: data.backgroundColor,
-                              textColor: data.textColor,
-                              icon: data.icon,
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ),
+                    // const SizedBox(height: 20),
+                    // Text(
+                    //   localizations.translate('home_statistics'),
+                    //   style: TextStyle(
+                    //     fontSize: 18,
+                    //     fontWeight: FontWeight.bold,
+                    //     letterSpacing: -1,
+                    //     color: Colors.grey[700],
+                    //   ),
+                    // ),
+                    // const SizedBox(height: 10),
+                    // SingleChildScrollView(
+                    //   scrollDirection: Axis.horizontal,
+                    //   physics: const BouncingScrollPhysics(),
+                    //   child: Row(
+                    //     children: dynamicReportCardDataList.map((data) {
+                    //       return Padding(
+                    //         padding: const EdgeInsets.symmetric(
+                    //           horizontal: 3.0,
+                    //         ),
+                    //         child: ReportCard(
+                    //           title: data.title,
+                    //           value: data.value,
+                    //           backgroundColor: data.backgroundColor,
+                    //           textColor: data.textColor,
+                    //           icon: data.icon,
+                    //         ),
+                    //       );
+                    //     }).toList(),
+                    //   ),
+                    // ),
                   ],
                 ),
               ),
